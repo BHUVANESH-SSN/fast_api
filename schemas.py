02fc8423220b22d67588c8ea81ba67f52105217f
BHUVANESH-SSN <bhuvaneshcsessn@gmail.com>
2025-10-18 17:42:16 +0530

    Initial FastAPI Project

---
+++ schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class TodoBase(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
+
+class TodoCreate(TodoBase):
+    pass
+
+class Todo(TodoBase):
+    id: int
+    model_config = { "from_attributes": True }  # <-- for Pydantic V2
